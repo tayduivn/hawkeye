daeb9dcbbb05be4a7e86464164fb65e57716bbd1
YoungDan_Hero <1292867089@qq.com>
2021-01-18 11:00:14 +0800

    1.18 11:00

--- src/app/pages/inspect-record-details/inspect-record-details.component.ts
+++ src/app/pages/inspect-record-details/inspect-record-details.component.ts
@@ -158,17 +158,6 @@ export class InspectRecordDetailsComponent implements OnInit {
                     this.plantVideo = [];
                 }
 
-                if (data.inspect_product_video && data.inspect_product_video.length != 0) {
-                    const str = JSON.stringify(data.inspect_product_video);
-                    this.productVideo = JSON.parse(str);
-                    this.productVideo = [];
-                    data.inspect_product_video.forEach(item => {
-                        this.productVideo.push(item.replace('storage/', ''));
-                    });
-                } else {
-                    this.productVideo = [];
-                }
-
                 if (data.inspect_showroom_video && data.inspect_showroom_video.length != 0) {
                     const str = JSON.stringify(data.inspect_showroom_video);
                     this.roomVideo = JSON.parse(str);
@@ -188,127 +177,10 @@ export class InspectRecordDetailsComponent implements OnInit {
                 data ? (this.factoryAssess = data) : (this.factoryAssess = {});
             });
         });
-        //   this.activatedRoute.queryParams
-        //   .pipe(
-        //       map(res => res.id),
-        //       switchMap(factory_id => this.inspecting.getAssessInfo({ factory_id })),
-        //   )
-        //   .subscribe(res => {
-        //       const { data } = res;
-        //       data ? (this.factoryAssess = data) : (this.factoryAssess = {});
-        //   });
-        // this.activatedRoute.queryParams
-        //     .pipe(
-        //         map(res => res.id),
-        //         switchMap(factory_id => this.inspecting.getFactoryDetails({ factory_id })),
-        //     )
-        //     .subscribe(res => {
-        //         const { data } = res;
-        //         this.data = data;
-        //         console.log(this.data);
-
-        //         // console.log(this.data.create_time);
-        //         this.data.create_time = this.handleTime(this.data.create_time);
-        //         if (data.rework_sample_pic && data.rework_sample_pic.length != 0) {
-        //             this.specimenPic = [];
-        //             data.rework_sample_pic.forEach(item => {
-        //                 // this.specimenPic = [];
-        //                 this.specimenPic.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.specimenPic = [];
-        //         }
-
-        //         data.sample ? (this.sampleProductList = data.sample) : (this.sampleProductList = {});
-        //         this.data.third_party = data.third_party - 0;
-        //         this.isHave_sample = data.sample.have_sample - 0;
-        //         this.isProvideSample = data.sample.will_supply - 0;
-        //         this.isSampleInformationShow = (this.isHave_sample && this.isProvideSample) - 0;
-        //         this.collaborativeProductList = data.simulation;
-        //         this.productList = data.product;
-        //         console.log(this.productList);
-
-        //         if (data.rework_business_license_pic && data.rework_business_license_pic.length != 0) {
-        //             const str = JSON.stringify(data.rework_business_license_pic);
-        //             this.businessPic = JSON.parse(str);
-        //             this.businessPic = [];
-        //             data.rework_business_license_pic.forEach(item => {
-        //                 this.businessPic.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.businessPic = [];
-        //         }
-
-        //         if (data.rework_facade_pic && data.rework_facade_pic.length != 0) {
-        //             const str = JSON.stringify(data.rework_facade_pic);
-        //             this.facadePic = JSON.parse(str);
-        //             this.facadePic = [];
-        //             data.rework_facade_pic.forEach(item => {
-        //                 this.facadePic.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.facadePic = [];
-        //         }
-
-        //         if (data.rework_plant && data.rework_facade_pic.length != 0) {
-        //             const str = JSON.stringify(data.rework_facade_pic);
-        //             this.plantPic = JSON.parse(str);
-        //             this.plantPic = [];
-        //             data.rework_plant.forEach(item => {
-        //                 this.plantPic.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.plantPic = [];
-        //         }
-
-        //         if (data.inspect_facade_video && data.inspect_facade_video.length != 0) {
-        //             const str = JSON.stringify(data.inspect_facade_video);
-        //             this.facadeVideo = JSON.parse(str);
-        //             this.facadeVideo = [];
-        //             data.inspect_facade_video.forEach(item => {
-        //                 this.facadeVideo.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.facadeVideo = [];
-        //         }
-
-        //         if (data.inspect_plant_video && data.inspect_plant_video.length != 0) {
-        //             const str = JSON.stringify(data.inspect_plant_video);
-        //             this.plantVideo = JSON.parse(str);
-        //             this.plantVideo = [];
-        //             data.inspect_plant_video.forEach(item => {
-        //                 this.plantVideo.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.plantVideo = [];
-        //         }
-
-        //         if (data.inspect_product_video && data.inspect_product_video.length != 0) {
-        //             const str = JSON.stringify(data.inspect_product_video);
-        //             this.productVideo = JSON.parse(str);
-        //             this.productVideo = [];
-        //             data.inspect_product_video.forEach(item => {
-        //                 this.productVideo.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.productVideo = [];
-        //         }
-
-        //         if (data.inspect_showroom_video && data.inspect_showroom_video.length != 0) {
-        //             const str = JSON.stringify(data.inspect_showroom_video);
-        //             this.roomVideo = JSON.parse(str);
-        //             this.roomVideo = [];
-        //             data.inspect_showroom_video.forEach(item => {
-        //                 this.roomVideo.push(item.replace('storage/', ''));
-        //             });
-        //         } else {
-        //             this.roomVideo = [];
-        //         }
-        //     });
     }
     showVideo(link) {
         this.isVideoShow = true;
-        this.videoUrl = environment.usFileUrl + link;
+        this.videoUrl = environment.usFileUrl + link.replace('storage/', '');
     }
     back() {
         this.flash.flash$.next('flash');
